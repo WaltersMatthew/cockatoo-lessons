39881896b0b76e3f81015474086ad3989bce7ad0
waltersmatthew <waltersmatthew88@gmail.com>
2023-03-01 12:13:14 -0800

    lesson

--- src/App.js
+++ src/App.js
@@ -2,52 +2,55 @@ import React, { useState, useEffect } from "react";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
 import "./App.css";
+import ajax from "ajax";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+    const [todoList, setTodoList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          data: {
-            todoList: JSON.parse(localStorage.getItem("savedTodoList")),
-          },
+    useEffect(() => {
+        new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({
+                    data: {
+                        todoList: JSON.parse(
+                            localStorage.getItem("savedTodoList")
+                        ),
+                    },
+                });
+            }, 2000);
+        }).then((result) => {
+            setTodoList(result.data.todoList);
+            setIsLoading(false);
         });
-      }, 2000);
-    }).then((result) => {
-      setTodoList(result.data.todoList);
-      setIsLoading(false);
-    });
-  }, []);
+    }, []);
 
-  useEffect(() => {
-    if (!isLoading) {
-      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-    }
-  }, [todoList, isLoading]);
+    useEffect(() => {
+        if (!isLoading) {
+            localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+        }
+    }, [todoList, isLoading]);
 
-  const addTodo = (newTodo) => {
-    setTodoList([...todoList, newTodo]);
-  };
+    const addTodo = (newTodo) => {
+        setTodoList([...todoList, newTodo]);
+    };
 
-  const removeTodo = (id) => {
-    const filteredTodoList = todoList.filter((todo) => todo.id !== id);
-    setTodoList(filteredTodoList);
-  };
+    const removeTodo = (id) => {
+        const filteredTodoList = todoList.filter((todo) => todo.id !== id);
+        setTodoList(filteredTodoList);
+    };
 
-  return (
-    <>
-      <h1>To Do List</h1>
-      <AddTodoForm onAddTodo={addTodo} />
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <TodoList onRemoveTodo={removeTodo} todoList={todoList} />
-      )}
-    </>
-  );
+    return (
+        <>
+            <h1>To Do List</h1>
+            <AddTodoForm onAddTodo={addTodo} />
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <TodoList onRemoveTodo={removeTodo} todoList={todoList} />
+            )}
+        </>
+    );
 }
 
 export default App;
